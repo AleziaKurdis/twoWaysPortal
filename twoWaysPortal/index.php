906a7ba84da18339e63ec42aab193bf9e46a8279
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-12-18 22:44:03 -0500

    Add files via upload

--- twoWaysPortal/index.php
+++ twoWaysPortal/index.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+<!--
+//  index.php
+//
+//  Created by Alezia Kurdis, December 15th 2021.
+//
+//  Vircadia's application to install 2 ways teleporters.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+-->
 <html>
     <head>
         <meta charset="utf-8">
--- twoWaysPortal/synchronize.php
+++ twoWaysPortal/synchronize.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: application/json');
 
-//  setPairingInfo.php
+//  synchronize.php
 //
 //  Created by Alezia Kurdis, December 15th 2021.
 //
@@ -16,11 +16,7 @@ if ((isset($_GET["pk"])) && (!empty($_GET["pk"]))){
     if ((isset($_GET["pid"])) && (!empty($_GET["pid"]))){
         $doorId = $_GET["pid"];
         
-        if ((isset($_GET["pln"])) && (!empty($_GET["pln"]))){
-            $placeName = $_GET["pln"];
-            
-            if ((isset($_GET["add"])) && (!empty($_GET["add"]))){
-                $landingAddress = $_GET["add"];
+
 
                 //read datafile
                 $dataFileName = "pairing.dat";
@@ -29,8 +25,7 @@ if ((isset($_GET["pk"])) && (!empty($_GET["pk"]))){
                 $updatedData = "";
                 $pairingEntries = explode("~", $data);
                 $first = true;
-                $foundAndUpdated = false;
-                //find the parining Key recorded and adjust the data
+                //find the parining Key recorded to return the response
                 //delete if outdated
                 foreach($pairingEntries as $key => $pairingEntry) {
                     $properties = explode("*", $pairingEntry);
@@ -50,28 +45,19 @@ if ((isset($_GET["pk"])) && (!empty($_GET["pk"]))){
                     }
                     
                     if (time() - $entryTimestamp < 86400) {
+                        $updatedData = $updatedData.$separator.$pairingEntry;
                         if ($entryPairingKey == $pairingKey) {
-                            if ($PortalID2 == "" && $PortalID1 != $doorId) {
-                                
-                                $updatedData = $updatedData.$separator.time()."*".$entryPairingKey."*".$PortalID1."*".$destinationUrl1."*";
-                                $updatedData = $updatedData.$destinationName1."*".$doorId."*".$landingAddress."*".$placeName;
-                                
-                            } else {
-                                $updatedData = $updatedData.$separator.$pairingEntry;
+                            if ($PortalID1 != $doorId) {
+                                $Response = '{"destinationUrl": "'.$destinationUrl2.'", "destinationName": "'.$destinationName2.'"}';
+                                echo($Response);
                             }
-                            
-                            
-                            $foundAndUpdated = true;
-                        } else {
-                            $updatedData = $updatedData.$separator.$pairingEntry;
+                            if ($PortalID2 != $doorId) {
+                                $Response = '{"destinationUrl": "'.$destinationUrl1.'", "destinationName": "'.$destinationName1.'"}';
+                                echo($Response);
+                            }
                         }                        
                     }
-                    
-                    
-                }
-                if ($foundAndUpdated == false) {
-                    //on ajoute une nouvelle entry
-                    $updatedData = $updatedData."~".time()."*".$pairingKey."*".$doorId."*".$landingAddress."*".$placeName."***"; 
+                     
                 }
                 
                 //save datafile
@@ -79,12 +65,7 @@ if ((isset($_GET["pk"])) && (!empty($_GET["pk"]))){
                 fwrite($fp, $updatedData);
                 fclose($fp);             
                 
-            }else{
-                echo("ERROR: No Landing Address.");
-            }            
-        }else{
-            echo("ERROR: No Place name.");
-        }
+
     }else{
         echo("ERROR: No door ID.");
     }
