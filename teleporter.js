237e353e1fa6e5a61a1f6268617e32d87a8dfdc5
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-12-20 22:51:47 -0500

    Add files via upload

--- teleporter.js
+++ teleporter.js
@@ -10,16 +10,29 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 (function(){ 
-
+    var jsMainFileName = "teleporter.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];    
+    
+    var TP_SOUND_URL = ROOT + "tp-sound.mp3";
+    var teleportSound;
+    
     var destinationData;
-
+    
     this.preload = function(entityID) {
         var properties = Entities.getEntityProperties(entityID, ["userData"]);
         destinationData = properties.userData;
+        
+        teleportSound = SoundCache.getSound(TP_SOUND_URL);
+        
     }
 
     this.enterEntity = function(entityID) {   
+        playSound();
         Window.location = destinationData;
     }     
+
+    function playSound() {
+        Audio.playSound(teleportSound, { volume: 0.3, localOnly: true });
+    };
 
 })
